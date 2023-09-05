1a280238a98edfebcbc16da1ca91e687b061431c
v.Bharathraj <bharathraj2105@gmail.com>
2023-09-05 19:36:00 +0530

    first commit

---
+++ color flipper/script.js
@@ -0,0 +1,21 @@
+const btn=document.getElementById("btn")
+const colortext=document.getElementById("color")
+const wrap =document.getElementById("wrap")
+const hex=[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+
+btn.addEventListener('click',changbg)
+
+function changbg(){
+    let hexcolor='#'
+    for(let i=1;i<=6;i++){
+   hexcolor+= randHexvalue()
+    }
+    wrap.style.backgroundColor=hexcolor
+    colortext.innerHTML=hexcolor
+}
+
+function randHexvalue(){
+    let randomIndex=Math.floor(Math.random()*16)
+return hex[randomIndex]
+
+}
